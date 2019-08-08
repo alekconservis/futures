9b24ea680aa09975a86b23476c78a13110026773
alekconservis <alukanen@conserviscorp.com>
2019-08-08 12:57:47 -0500

    basic user page is up; no authentication yet.

--- futures/website/views.py
+++ futures/website/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        print('- post')
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print(f'-- username: {username}')
+        print(f'-- password: {password}')
+        return render(request, 'login.html',
+                      {"item": "hello from django app",
+                       "username": username})
+    else:
+        print('- get')
+        return render(request, 'login.html',
+                        {"item": "hello from django app"})
