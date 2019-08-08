b6dde51a25477460c91703bd52a031ead578de40
Max Marti <max@foundrymakes.com>
2019-08-08 15:40:59 -0500

    add fields

--- futures/website/models.py
+++ futures/website/models.py
@@ -1,26 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
-
 class User(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     cash_balance = models.FloatField(default=100.0)
-
 
 # Create your models here.
 class Product(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
     expires_at = models.DateField('expiration date')
     price = models.FloatField()
-
+    unit = models.CharField(max_length=25)
+    default_quantity = models.IntegerField('quantity')
     def __str__(self):
         return self.name
 
-
 class Contract(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     seller = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='sell_contract')
     buyer = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='buy_contract')
     product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     description = models.CharField(max_length=255)
-    end_date = models.DateField()
+    end_date = models.DateField('maturity date')
