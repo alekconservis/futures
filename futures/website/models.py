c79d9b75e3928094421693199d3c11910c29c4b2
Max Marti <max@foundrymakes.com>
2019-08-08 13:54:12 -0500

    add contract model and view

--- futures/website/models.py
+++ futures/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contract(models.Model):
+    # seller = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    # buyer = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    description = models.CharField(max_length=255)
+    end_date = models.DateField()
--- futures/website/views.py
+++ futures/website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Contract
 
 # Create your views here.
+def index(request):
+  return HttpResponse("Hello, world. You're at the polls index.")
+
+def contracts(request):
+    contract_list = Contract.objects.order_by('-end_date')[:5]
+    context = {
+      'contracts': contract_list
+    }
+    return render(request, 'website/contracts.html', context)
